f50290edf9bad16a7f5a591681911587778637d8
D McKnight <dmcknight@fosforus.com>
2020-09-10 16:16:15 -0500

    update

--- website/static/static/js/version.js
+++ website/static/static/js/version.js
@@ -1,5 +1,5 @@
-const presto_latest_presto_version = '0.237.2';
-const presto_latest_num_commits = 157;
-const presto_latest_num_contributors = 29;
-const presto_latest_num_committers = 11;
-const presto_latest_date = 'August 12, 2020';
+const presto_latest_presto_version = '0.240';
+const presto_latest_num_commits = 172;
+const presto_latest_num_contributors = 33;
+const presto_latest_num_committers = 12;
+const presto_latest_date = 'August 31, 2020';
