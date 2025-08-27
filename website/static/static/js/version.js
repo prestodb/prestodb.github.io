93ab510e16851831586edd58f72e096f06d1c851
prestodb-ci <ci@lists.prestodb.io>
2025-08-27 19:00:24 +0000

    Add 0.294 docs

--- website/static/static/js/version.js
+++ website/static/static/js/version.js
@@ -1,5 +1,5 @@
-const presto_latest_presto_version = '0.293';
-const presto_latest_num_commits = 228;
-const presto_latest_num_contributors = 81;
-const presto_latest_num_committers = 40;
-const presto_latest_date = 'June 13, 2025';
+const presto_latest_presto_version = '0.294';
+const presto_latest_num_commits = 260;
+const presto_latest_num_contributors = 76;
+const presto_latest_num_committers = 41;
+const presto_latest_date = 'August 27, 2025';
